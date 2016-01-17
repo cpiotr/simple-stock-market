4a2ead5d60fc71bd417360eeb9618b19e5cb56f1
c.piotre <c.piotre@gmail.com>
2016-01-17 01:11:54 +0100

    Implement PE Ratio calculation

--- src/main/java/pl/ciruk/stockmarket/stock/Stock.java
+++ src/main/java/pl/ciruk/stockmarket/stock/Stock.java
@@ -1,11 +1,14 @@
 package pl.ciruk.stockmarket.stock;
 
+import com.google.common.base.Preconditions;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
+import pl.ciruk.stockmarket.math.Decimals;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Getter
 @ToString
@@ -18,4 +21,13 @@ public abstract class Stock {
 	BigDecimal parValueInPennies;
 
 	public abstract BigDecimal calculateDividendYieldFor(BigDecimal priceInPennies);
+
+	public BigDecimal calculatePriceEarningsRatioFor(BigDecimal priceInPennies) {
+		Preconditions.checkArgument(priceInPennies != null, "Price cannot be null");
+		Preconditions.checkArgument(priceInPennies.compareTo(BigDecimal.ZERO) != 0, "Price cannot be equal to zero");
+		BigDecimal normalizedPrice = Decimals.applyDefaultScaleTo(priceInPennies);
+
+		return calculateDividendYieldFor(normalizedPrice)
+				.divide(priceInPennies, RoundingMode.HALF_UP);
+	}
 }
---
+++ src/test/java/pl/ciruk/stockmarket/stock/StockTest.java
@@ -0,0 +1,52 @@
+package pl.ciruk.stockmarket.stock;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static pl.ciruk.stockmarket.math.Decimals.applyDefaultScaleTo;
+
+public class StockTest {
+
+	@Test
+	public void shouldCallDividendYieldCalculationDuringPERatioCalculation() throws Exception {
+		// Given
+		Stock stock = mockStock();
+
+		// When
+		stock.calculatePriceEarningsRatioFor(BigDecimal.ONE);
+
+		// Then
+		verify(stock).calculateDividendYieldFor(applyDefaultScaleTo(BigDecimal.ONE));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailWhenPriceIsEmpty() throws Exception {
+		// Given
+		Stock stock = mockStock();
+		BigDecimal emptyPrice = null;
+
+		// When
+		stock.calculatePriceEarningsRatioFor(emptyPrice);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailWhenPriceIsZero() throws Exception {
+		// Given
+		Stock stock = mockStock();
+
+		// When
+		stock.calculatePriceEarningsRatioFor(BigDecimal.ZERO);
+	}
+
+	private Stock mockStock() {
+		Stock stock = Mockito.mock(Stock.class);
+		when(stock.calculatePriceEarningsRatioFor(any(BigDecimal.class))).thenCallRealMethod();
+		when(stock.calculateDividendYieldFor(any(BigDecimal.class))).thenReturn(BigDecimal.ZERO);
+		return stock;
+	}
+}
