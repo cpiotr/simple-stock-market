77dbbb73ef9be4b8e218e92823e2764c06c82d89
c.piotre <c.piotre@gmail.com>
2016-01-17 21:59:38 +0100

    Mirror all stock calculations in market

--- src/main/java/pl/ciruk/stockmarket/Market.java
+++ src/main/java/pl/ciruk/stockmarket/Market.java
@@ -4,6 +4,8 @@ import com.google.common.base.Preconditions;
 import pl.ciruk.stockmarket.stock.Stock;
 import pl.ciruk.stockmarket.trade.Trade;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,10 +17,34 @@ public class Market {
 		Preconditions.checkArgument(trade != null, "Trade cannot be null");
 
 		String stock = trade.getStock();
-		Preconditions.checkArgument(stock != null, "Trade's stock cannot be null");
-		Preconditions.checkState(stocks.containsKey(stock), "Stock %s is not registered on the market", stock);
+		checkStock(stock);
 
-		stocks.get(stock).record(trade);
+		stocks.get(stock)
+				.record(trade);
+	}
+
+	public BigDecimal calculateRecentWeightedPriceFor(String stock) {
+		checkStock(stock);
+
+		LocalDateTime quarterAgo = LocalDateTime.now().minusMinutes(15);
+		return stocks.get(stock)
+				.calculateVolumeWeightedPrice(
+						trade -> trade.getTimestamp().isAfter(quarterAgo)
+				);
+	}
+
+	public BigDecimal calculateDividendYieldFor(String stock, BigDecimal price) {
+		checkStock(stock);
+
+		return stocks.get(stock)
+				.calculateDividendYieldFor(price);
+	}
+
+	public BigDecimal calculatePriceEarningRatioFor(String stock, BigDecimal price) {
+		checkStock(stock);
+
+		return stocks.get(stock)
+				.calculatePriceEarningsRatioFor(price);
 	}
 
 	public Optional<Stock> findStockFor(String symbol) {
@@ -29,5 +55,10 @@ public class Market {
 
 	public void register(Stock stock) {
 		stocks.putIfAbsent(stock.getSymbol(), stock);
+	}
+
+	private void checkStock(String stock) {
+		Preconditions.checkArgument(stock != null, "Trade's stock cannot be null");
+		Preconditions.checkState(stocks.containsKey(stock), "Stock %s is not registered on the market", stock);
 	}
 }
